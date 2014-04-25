b0b55991a6634c2934f8b1a0b49e739746cf9735
Ran Klein <ranklein86@gmail.com>
2014-04-25 14:09:49 +0300

    aaaa

--- public_html/server.js
+++ public_html/server.js
@@ -3,5 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var express = require('express');
 
 
+var app = express();//.createServer();
+app.configure(function () {
+    app.use(express.logger('dev'));    
+    app.use(express.bodyParser());//it will parse json request bodies (as well as others), and place the result in req.body:
+});
+
+app.listen(3000);
